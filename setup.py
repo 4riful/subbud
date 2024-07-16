a033402a7e6ee2b994d91322e296f4ab4dc88f6c
Ariful Anik <45453772+4riful@users.noreply.github.com>
2024-07-17 00:07:51 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="subbud",
-    version="1.0.2",
-    description="Bug bounty subdomain management tool",
-    author="XETTABYTE",
-    author_email="ariful4nik@gmail.com",
-    packages=find_packages(),
-    install_requires=["redis"],
-    entry_points={
-        "console_scripts": [
-            "subbud = subbud.main:main"
-        ]
+    version="0.0.1",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A short description of the package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/4riful/subbud",
+    project_urls={
+        "Bug Tracker": "https://github.com/4riful/subbud/issues",
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    python_requires=">=3.6",
 )
